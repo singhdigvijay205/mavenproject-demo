c6c85dbd803d4f2c1a4da309c6d4a947ff6fcfb9
singhdigvijay205 <singhdigvijay205@gmail.com>
2020-04-28 08:47:25 -0400

    chekcking polling

--- mavendemo/src/main/java/com/learnmaven/mavendemo/App.java
+++ mavendemo/src/main/java/com/learnmaven/mavendemo/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
     	Calculator calc = new Calculator();
-    	LOGGER.debug(calc.add(1, 5));
+    	LOGGER.debug(calc.add(1, 7));
     	LOGGER.debug(calc.subtract(1, 2));
     	LOGGER.debug(calc.multiply(1, 2));
     	LOGGER.debug(calc.divide(1, 2));
